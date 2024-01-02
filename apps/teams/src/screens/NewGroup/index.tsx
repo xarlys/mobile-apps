29d74ac660b6bce2176627ce30486232eada45ad
xarlys <xarlysouza@hotmail.com>
2024-01-02 18:10:51 -0300

    feat prevent glitch navigation

--- apps/teams/src/screens/NewGroup/index.tsx
+++ apps/teams/src/screens/NewGroup/index.tsx
@@ -1,21 +1,24 @@
-import { Container, Content, Icon } from "./styles";
-
-import { Header } from "@components/Header";
-import { Button } from "@components/Button";
-import { Highlight } from "@components/Highlight";
-import { Input } from "@components/Input";
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
-const NewGroup = () => {
-  const navigation = useNavigation();
-  const hadleNew = () => {
-    navigation.navigate('players', { group: 'Rocket' });
+import { Container, Content, Icon } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+import { Header } from "@components/Header";
+import { Button } from "@components/Button";
+
+export const NewGroup = () => {
+  const [group, setGroup] = useState('')
+
+  const navigation = useNavigation()
+
+  function handleNew() {
+    navigation.navigate('players', { group })
   }
 
   return (
     <Container>
       <Header showBackButton />
-
       <Content>
         <Icon />
         
@@ -24,18 +27,17 @@ const NewGroup = () => {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input 
+        <Input
           placeholder="Nome da turma"
+          onChangeText={setGroup}
         />
 
         <Button 
           title="Criar"
           style={{ marginTop: 20 }}
-          onPress={hadleNew}
+          onPress={handleNew}
         />
       </Content>
     </Container>
   )
 }
-
-export { NewGroup }
